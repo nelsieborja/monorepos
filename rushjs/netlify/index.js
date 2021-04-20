02bf40d4270573af90d27a5885e1d6a719db915e
Nelsie Borja <nelsieborja@gmail.com>
2021-04-20 12:15:18 +0200

    split netlify commands

--- rushjs/netlify/index.js
+++ rushjs/netlify/index.js
@@ -1,5 +1,7 @@
 module.exports = {
   onPreBuild: async ({ utils: { build, status, cache, run, git } }) => {
-    await run.command("npm i -g @microsoft/rush && rush install && rush build");
+    await run.command("npm i -g @microsoft/rush");
+    await run.command("rush install");
+    await run.command("rush build");
   },
 };
